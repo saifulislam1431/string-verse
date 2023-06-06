c61a1e612c25ccf5ee81b789b91849144d6a686b
Saiful Islam <sakibbhaiya143@gmail.com>
2023-06-07 00:02:58 +0600

    Theme and style setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        lightThem: {
+          "primary": "#3b7197",
+          "secondary": "#4a8db7",
+          "accent": "#74bde0",
+          "neutral": "#28282B ",
+          "error" : "#EE0C36",
+          "base-100": "#eaebed",
+        },
+      },
+      "forest",
+    ],
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
 }
 
