a7fcfe949c81984e09cb762048729a0151aae33f
Saiful Islam <sakibbhaiya143@gmail.com>
2023-06-07 23:41:51 +0600

    popular class section complete

---
+++ src/Pages/Home/PopularClasses/PopularClass.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PopularClass = ({item}) => {
+    console.log(item);
+    const{className,image,numberOfStudents}=item;
+    return (
+        <section>
+            <div className="card w-80 bg-base-100 shadow-xl">
+  <figure><img src={image} alt="Class Image" className='h-60 w-full'/></figure>
+  <div className="px-4 py-2">
+    <h2 className="brandTitle">
+      {className}
+    </h2>
+  </div>
+  <div className="badge badge-primary absolute right-2 top-2 text-red-600 font-semibold">{numberOfStudents}+ Students</div>
+</div>
+        </section>
+    );
+};
+
+export default PopularClass;
--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Fade, Slide } from "react-awesome-reveal";
+import useClasses from '../../../Hooks/useClasses';
+import PopularClass from './PopularClass';
+
 
 const PopularClasses = () => {
+    const[classes] = useClasses();
 
     return (
         <section className='my-14'>
@@ -10,6 +14,17 @@ const PopularClasses = () => {
   <p className='text-center font-bold text-2xl text-secondary'>Unleash Your Musical Potential with Our Hottest Classes</p>
   <p className='text-center my-3 font-semibold'>Unlock Your Musical Potential with our Hottest Classes! Join Now and Elevate Your Skills!</p>
 </Slide>
+
+<div className='flex items-center justify-center'>
+<div className='my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+{
+    classes.slice(0,6).map(item=><PopularClass
+    key={item._id}
+    item={item}
+    ></PopularClass>)
+}
+</div>
+</div>
         </section>
     );
 };
