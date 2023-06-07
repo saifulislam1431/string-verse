850f697f745de5a354ba02ea78e9303fa872094c
Saiful Islam <sakibbhaiya143@gmail.com>
2023-06-07 22:38:30 +0600

    popular class section start

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Fade, Slide } from "react-awesome-reveal";
 
 const PopularClasses = () => {
+
     return (
         <section className='my-14'>
 
+<Slide triggerOnce delay={3}>
+  <p className='text-center font-bold text-2xl text-secondary'>Unleash Your Musical Potential with Our Hottest Classes</p>
+  <p className='text-center my-3 font-semibold'>Unlock Your Musical Potential with our Hottest Classes! Join Now and Elevate Your Skills!</p>
+</Slide>
         </section>
     );
 };
