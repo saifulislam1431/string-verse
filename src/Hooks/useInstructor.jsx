2680a950076fa70ce1402050f1ac6ee91614129b
Saiful Islam <sakibbhaiya143@gmail.com>
2023-06-10 00:26:01 +0600

    Instructor hook added

---
+++ src/Hooks/useInstructor.jsx
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import useAuth from './useAuth';
+import useAxiosSecure from './useAxiosSecure';
+
+const useInstructor = () => {
+    const { user, loading } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+
+    const {data: isInstructor, isLoading: isInstructorLoading } = useQuery({
+        queryKey: ["isInstructor", user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            if(user){
+                const res = await axiosSecure.get(`/users/instructor/${user?.email}`)
+                return res.data.instructor
+            }
+        }
+    })
+    return[isInstructor , isInstructorLoading]
+};
+
+export default useInstructor;
--- src/Pages/AdminDashboard/ManageClass/ManageClass.jsx
+++ src/Pages/AdminDashboard/ManageClass/ManageClass.jsx
@@ -11,7 +11,7 @@ const ManageClass = () => {
             subTitle="Manage Classes"
             ></SectionTitle>
 
-            <div className='lg:mx-10'>
+            <div>
             <div className="overflow-x-auto">
   <table className="table">
     {/* head */}
@@ -23,6 +23,7 @@ const ManageClass = () => {
         <th>Instructor email</th>
         <th>Available seats</th>
         <th>Price</th>
+        <th>Status</th>
         <th>Action</th>
       </tr>
     </thead>
@@ -47,20 +48,32 @@ const ManageClass = () => {
         <td className='font-semibold'>{singleClass?.instructorEmail}</td>
         <td className='font-semibold text-center'>{singleClass?.availableSeats}</td>
         <td className='font-semibold text-center'>${singleClass?.price}</td>
+        <td className='font-semibold text-center'>{singleClass?.status}</td>
         <th className='flex flex-col items-start justify-start'>
           <button className='myBtn' disabled={singleClass?.status === "approved" || singleClass?.status === "deny" ? true :false}>Approve</button>
           <button className='myBtnSec' disabled={singleClass?.status === "approved" || singleClass?.status === "deny" ? true :false}>Deny</button>
-          <button className='myBtnThr' >Feedback</button>
+          {/* <button className='myBtnThr' >Feedback</button> */}
+          <label htmlFor="my_modal_6" className="myBtnThr">Feedback</label>
         </th>
       </tr>)
       }
 
     </tbody>
 
-    
   </table>
 </div>
             </div>
+            
+    <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Write Feedback</h3>
+    <textarea className='inputField mt-4' name="message" id="" cols="10" rows="10" placeholder='Feedback'></textarea>
+    <div className="modal-action">
+      <label htmlFor="my_modal_6" className="myBtnSec">Send</label>
+    </div>
+  </div>
+</div>
         </section>
     );
 };
