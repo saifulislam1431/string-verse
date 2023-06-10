250a977987509afc02634dd5a50b678c44a44605
Saiful Islam <sakibbhaiya143@gmail.com>
2023-06-11 01:30:28 +0600

    Enroll classes hook added

---
+++ src/Hooks/useEnroll.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import useAuth from './useAuth';
+import useAxiosSecure from './useAxiosSecure';
+
+const useEnroll = () => {
+    const {user , loading} = useAuth()
+    const [axiosSecure] = useAxiosSecure()
+    const{data: enrollClasses = [] , refetch}=useQuery({
+        queryKey:['enroll' , user?.email],
+        enabled: !loading && !!user?.email,
+        queryFn:async()=>{
+            const res = await axiosSecure.get(`/enrollDetails?email=${user?.email}`);
+            return res.data;
+        }
+    })
+    return[enrollClasses,refetch]
+};
+
+export default useEnroll;
--- src/Pages/UserDashboard/EnrolledClasses/EnrolledClasses.jsx
+++ src/Pages/UserDashboard/EnrolledClasses/EnrolledClasses.jsx
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
-import useAxiosSecure from '../../../Hooks/useAxiosSecure';
-import useAuth from '../../../Hooks/useAuth';
 import { HiOutlinePlay } from "react-icons/hi2";
+import useEnroll from '../../../Hooks/useEnroll';
 
 
 const EnrolledClasses = () => {
-    const { user, loading } = useAuth()
-    const [axiosSecure] = useAxiosSecure();
-    const [enrollClasses , setEnrollClasses] = useState([]);
 
-    useEffect(() => {
-        axiosSecure.get(`/enrollDetails?email=${user?.email}`)
-            .then(res => {
-                setEnrollClasses(res.data);
-            })
-    }, [])
+    const [enrollClasses] = useEnroll();
+
 
 
     return (
@@ -23,28 +15,28 @@ const EnrolledClasses = () => {
             <SectionTitle
                 subTitle="Enrolled Classes"
             ></SectionTitle>
-<div className='flex items-center justify-center lg:mx-5'>
+            <div className='flex items-center justify-center lg:mx-5'>
 
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-        {
-enrollClasses.map(enrollClass=>            
-    <div key={enrollClass._id} className="card card-compact w-84 bg-base-100 shadow-xl">
-        <figure><img src={enrollClass?.image} alt="Image" className='h-40 w-full'/></figure>
-        <div className="card-body">
-            <h2 className="card-title">{enrollClass?.className}</h2>
-            <div className='space-y-3 mb-3'>
-                <p className='font-semibold'>Instructor Name: {enrollClass?.instructorName}</p>
-                <p className='font-semibold'>Instructor Email: {enrollClass?.instructorEmail}</p>
-            </div>
-            <div className="card-actions justify-end">
-                <button className="bg-primary px-2 py-2 rounded-full"><HiOutlinePlay className='h-6 w-6 text-white'/></button>
-            </div>
-        </div>
-    </div>)
-        }
-    </div>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+                    {
+                        enrollClasses.map(enrollClass =>
+                            <div key={enrollClass._id} className="card card-compact w-84 bg-base-100 shadow-xl">
+                                <figure><img src={enrollClass?.image} alt="Image" className='h-40 w-full' /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{enrollClass?.className}</h2>
+                                    <div className='space-y-3 mb-3'>
+                                        <p className='font-semibold'>Instructor Name: {enrollClass?.instructorName}</p>
+                                        <p className='font-semibold'>Instructor Email: {enrollClass?.instructorEmail}</p>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        <button className="bg-primary px-2 py-2 rounded-full"><HiOutlinePlay className='h-6 w-6 text-white' /></button>
+                                    </div>
+                                </div>
+                            </div>)
+                    }
+                </div>
 
-</div>
+            </div>
         </section>
     );
 };
